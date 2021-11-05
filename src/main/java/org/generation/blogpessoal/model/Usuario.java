d3cebe292277668daa155882313a353c4a55a5f0
byTiagoAssis <tiagoassis0594@gmail.com>
2021-11-04 21:30:06 -0300

    correçoes de bugs

--- src/main/java/org/generation/blogpessoal/model/Usuario.java
+++ src/main/java/org/generation/blogpessoal/model/Usuario.java
@@ -16,74 +16,119 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private long id;
-@NotNull(message = "O atributo nome é obrigatório")
-@Size(min = 5, max = 100,
-message = "O atributo nome deve ter no mínimo 05 e no máximo 100 caracteres")
-private String nome;
-@NotNull(message = "O atributo usuário é obrigatório")
-@NotBlank(message = "O atributo usuário não pode ser vazio")
-@Email(message = "O atributo usuário deve ser um email")
-private String usuario;
-@NotNull(message = "O atributo senha é obrigatório")
-@Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres")
-private String senha;
-@Column(name = "dt_nascimento")
-@JsonFormat(pattern="yyyy-MM-dd")
-private LocalDate dataNascimento;
-@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
-@JsonIgnoreProperties("usuario")
-private List <Postagem> postagem;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo nome deve ter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@ApiModelProperty(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
+	private String usuario;
+	
+	@NotNull(message = "O atributo senha é obrigatório")
+	@Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres")
+	private String senha;
+	
+	@Column(name = "dt_nascimento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataNascimento;
+	
+	private String foto;
+	
+	private String tipo;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+	
+	
+
 // Primeiro método Construtor - Com os atributos
-public Usuario(long id, String nome, String usuario, String senha,
-LocalDate dataNascimento) {
-this.id = id;
-this.nome = nome;
-this.usuario = usuario;
-this.senha = senha;
-this.dataNascimento = dataNascimento;
-}
+	public Usuario(long id, String nome, String usuario, String senha, LocalDate dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.dataNascimento = dataNascimento;
+	}
+
 // Segundo método Construtor - Sem os atributos
-public Usuario() { }
-public long getId() {
-return this.id;
-}
-public void setId(long id) {
-this.id = id;
-}
-public String getNome() {
-return this.nome;
-}
-public void setNome(String nome) {
-this.nome = nome;
-}
-public String getUsuario() {
-return this.usuario;
-}
-public void setUsuario(String usuario) {
-this.usuario = usuario;
-}
-public String getSenha() {
-return this.senha;
-}
-public void setSenha(String senha) {
-this.senha = senha;
-}
-public LocalDate getDataNascimento() {
-return this.dataNascimento;
-}
-public void setDataNascimento(LocalDate dataNascimento) {
-this.dataNascimento = dataNascimento;
-}
-public List<Postagem> getPostagem() {
-return this.postagem;
-}
-public void setPostagem(List<Postagem> postagem) {
-this.postagem = postagem;
-}
+	public Usuario() {
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public LocalDate getDataNascimento() {
+		return this.dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Postagem> getPostagem() {
+		return this.postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
 }
